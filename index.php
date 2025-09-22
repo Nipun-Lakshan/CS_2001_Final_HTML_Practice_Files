208f7c3ea4a0d88402ce95dd007b484332022b70
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-09-22 22:45:41 +0530

    Practice 10

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+// Enable error reporting for development environment.
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Display a Text on Webpage
+echo "Hi! I am a PHP Script!";
+print "<br />";
+echo "Hello, ";
+print "<br />";
+echo "<b>I'm here!</b>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--  -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practice 10</title>
+</head>
+<body>
+    <?php
+    // PHP with HTML - Simple Code
+    echo "<p> Hello World! </p>";
+    ?>
+    <?php
+    $variable = 10;
+    echo "$variable <br />";
+    ?>
+</body>
+</html>
+
+<?php
+// Print Statement on Webpage
+// Single Line Comment
+/* 
+Multi Line Comment
+Multi Line Comment
+*/
+// Variable Declaration and Initialization
+$txt = "Hello, World!";
+$x = 16;
+print "Hello, ";
+print "World! <br/>";
+print "$txt <br/>";
+print "$x <br/>";
+?>
